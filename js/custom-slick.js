8d38f09c0fa50697b75b5315ae6622f939aaab67
MiriyalaMuraliKrishna <muralikrishna.mca35@gmail.com>
2023-05-05 17:24:17 +0530

    added

--- js/custom-slick.js
+++ js/custom-slick.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
         slidesToScroll: 1,
         dots: false,
         infinite: false,
+        variableWidth: true,
         arrows: true,
         prevArrow: '<div class="slick-arrow slick-prev"><span class="slick-btn fa-regular fa-arrow-left flex flex-center"></span></div>',
         nextArrow: '<div class="slick-arrow slick-next"><span class="slick-btn fa-regular fa-arrow-right flex flex-center"></span></div>',
