8dbf58f6bff88431edd919ab2d0be73ed5ac5f3f
MiriyalaMuraliKrishna <muralikrishna.mca35@gmail.com>
2023-05-05 15:37:51 +0530

    added index.html

---
+++ js/custom-slick.js
@@ -0,0 +1,22 @@
+
+var $ = jQuery.noConflict();
+$(document).ready(function(){
+    $('.glance-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        infinite: false,
+        arrows: true,
+        prevArrow: '<div class="slick-arrow slick-prev"><span class="slick-btn fa-regular fa-arrow-left flex flex-center"></span></div>',
+        nextArrow: '<div class="slick-arrow slick-next"><span class="slick-btn fa-regular fa-arrow-right flex flex-center"></span></div>',
+        focusOnSelect: true,
+        responsive: [
+            {
+            breakpoint: 1023,
+                settings: {
+                    arrows: false,
+                }
+            },
+        ],
+    });
+});
