4bfb98fb7d9f63c2d2672e6b1cf362e2db625934
MuraliJbk <murali@jbkinfotech.com>
2023-05-16 19:19:33 +0530

    50% done

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,19 +1,18 @@
+
+jQuery(window).on('load scroll', function(){
+    let scroll = jQuery(this).scrollTop();
+    scroll > 4 ? jQuery('.main-header').addClass('fixed_header') : jQuery('.main-header').removeClass('fixed_header');
+});
+
 jQuery(document).ready(function(){
-
-    jQuery(window).on('scroll load', function(){
-        let scroll = jQuery(this).scrollTop();
-        scroll > 4 ? jQuery('.main-header').addClass('fixed_header') : jQuery('.main-header').removeClass('fixed_header');
-    });
-
     jQuery('.toggle_menu').on('click', function(e){
         e.preventDefault();
         jQuery(this).toggleClass('active');
         jQuery('.main-header').toggleClass('sticky_header');
         jQuery('.navigation').toggleClass('open');
-
     });
     jQuery('ul.main_menu > li > a').on('click', function(e){
-      //  e.preventDefault();
+        e.preventDefault();
         jQuery(this).parent('li').addClass('current-menu-item');
         jQuery(this).parent().siblings('li').removeClass('current-menu-item');
         jQuery('.toggle_menu').removeClass('active');
--- js/custom-slick.js
+++ js/custom-slick.js
@@ -18,17 +18,31 @@ jQuery(document).ready(function(){
                 }
             },
         ],
+    });    
+});
+
+jQuery(window).on('load resize orientationchange', function() {
+    jQuery('.table-annual-slider').each(function(){
+        let tableSlide = $(this);
+        if (jQuery(window).width() >= 768) {
+            if (tableSlide.hasClass('slick-initialized')) {
+                tableSlide.slick('unslick');
+            }
+        }
+        else{
+            if (!tableSlide.hasClass('slick-initialized')) {
+                tableSlide.slick({
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    mobileFirst: true,
+                    arrows: true,
+                    prevArrow: '<div class="slick-arrow slick-prev"><span class="slick-btn fa-regular fa-arrow-left flex flex-center"></span></div>',
+                    nextArrow: '<div class="slick-arrow slick-next"><span class="slick-btn fa-regular fa-arrow-right flex flex-center"></span></div>',
+                    focusOnSelect: true,
+                    infinite: false,
+                    dots: false,
+                });
+            }
+        }
     });
-    if(jQuery(window).width()<=767){
-        jQuery('.table-annual-slider').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: false,
-            infinite: false,
-            arrows: true,
-            prevArrow: '<div class="slick-arrow slick-prev"><span class="slick-btn fa-regular fa-arrow-left flex flex-center"></span></div>',
-            nextArrow: '<div class="slick-arrow slick-next"><span class="slick-btn fa-regular fa-arrow-right flex flex-center"></span></div>',
-            focusOnSelect: true,
-        });
-    }
 });
