77a15ff9837e5047a5b208572f9606fabf948347
MuraliJbk <murali@jbkinfotech.com>
2023-05-27 17:24:23 +0530

    animation adde

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,36 +1,43 @@
 
-jQuery(window).on("load scroll", function(){
-    let scroll = jQuery(this).scrollTop();
-    scroll > 4 ? jQuery(".main-header").addClass("fixed_header") : jQuery(".main-header").removeClass("fixed_header");
+var $ = jQuery.noConflict();
+$(window).on("load scroll", function(){
+    let scroll = $(this).scrollTop();
+    scroll > 4 ? $(".main-header").addClass("fixed_header") : $(".main-header").removeClass("fixed_header");
 });
+$(document).ready(function(){
+    $(".toggle_menu").on("click", function(e){
+        e.preventDefault();
+        $(this).toggleClass("active");
+        $(".main-header").toggleClass("sticky_header");
+        $(".navigation").toggleClass("open");
+    });
+    $("ul.main_menu > li > a").on("click", function(e){
+        e.preventDefault();
+        $(this).parent("li").addClass("current-menu-item");
+        $(this).parent().siblings('li').removeClass('current-menu-item');
+        // $(".toggle_menu").removeClass("active");
+        // $(".main-header").removeClass("sticky_header");
+        // $(".navigation").removeClass("open");
+        $(this).parent().siblings("li").find("ul").slideUp();
+        $(this).siblings("ul").slideToggle();
+    });
+    let level2 = $("ul.main_menu li > ul > li.menu-item-has-children > a");
+    level2.on("click", function(){
+        $(this).parent().siblings("li.menu-item-has-children").find("ul").slideUp();
+        $(this).siblings("ul").slideToggle();
+    });
+    $(".accordion-header").on("click", function(e){
+        e.preventDefault();
+        $(this).parent().toggleClass("active");
+        $(this).parent().siblings().removeClass("active");
+        $(this).parent().siblings().find(".accordion-header").removeClass("open");
+        $(this).toggleClass("open");
+        $(this).siblings(".accordion-content").slideToggle(500);
+        $(this).parent().siblings().find(".accordion-content").slideUp(500);
+    });
 
-jQuery(document).ready(function(){
-    jQuery(".toggle_menu").on("click", function(e){
-        e.preventDefault();
-        jQuery(this).toggleClass("active");
-        jQuery(".main-header").toggleClass("sticky_header");
-        jQuery(".navigation").toggleClass("open");
-    });
-    jQuery("ul.main_menu > li > a").on("click", function(e){
-        e.preventDefault();
-        jQuery(this).parent("li").addClass("current-menu-item");
-        jQuery(this).parent().siblings('li').removeClass('current-menu-item');
-        jQuery(".toggle_menu").removeClass("active");
-        jQuery(".main-header").removeClass("sticky_header");
-        jQuery(".navigation").removeClass("open");
-    });
-    jQuery(".accordion-header").on("click", function(e){
-        e.preventDefault();
-        jQuery(this).parent().toggleClass("active");
-        jQuery(this).parent().siblings().removeClass("active");
-        jQuery(this).parent().siblings().find(".accordion-header").removeClass("open");
-        jQuery(this).toggleClass("open");
-        jQuery(this).siblings(".accordion-content").slideToggle(500);
-        jQuery(this).parent().siblings().find(".accordion-content").slideUp(500);
-    });
-
-    if(jQuery(window).width() >= 768){
-        jQuery(".team-member-list").each(function() {
+    if($(window).width() >= 768){
+        $(".team-member-list").each(function() {
             let $this = $(this);
             let descItem = $this.find(".team-member-desc");
             $this.hover(function() {
@@ -40,24 +47,23 @@ jQuery(document).ready(function(){
             });
         });
     }
-    if(jQuery(window).width() <= 767){
-        jQuery(".team-member-pos").on("click", function() {
-            jQuery(this).children(".team-member-arrow").addClass("active");
-            jQuery(this).parents().siblings().find(".team-member-desc").slideUp();
-            jQuery(this).siblings(".team-member-desc").slideToggle();
+    if($(window).width() <= 767){
+        $(".team-member-pos").on("click", function() {
+            $(this).children(".team-member-arrow").addClass("active");
+            $(this).parents().siblings().find(".team-member-desc").slideUp();
+            $(this).siblings(".team-member-desc").slideToggle();
         });
     }
-
 });
 
-jQuery(document).ready(function(){
-    let headerHeight = jQuery(".main-header").outerHeight(true);
-    jQuery("a[href*=#]:not([href=#])").click(function(event){
+$(document).ready(function(){
+    let headerHeight = $(".main-header").outerHeight(true);
+    $("a[href*=#]:not([href=#])").click(function(event){
         event.preventDefault();
-        let myattr = jQuery(this).attr("href");
-        let target = jQuery(myattr);
+        let myattr = $(this).attr("href");
+        let target = $(myattr);
         if (target.length) {
-            jQuery("html, body").animate({
+            $("html, body").animate({
                 scrollTop: target.offset().top - headerHeight
             }, 2000);
             return false;
